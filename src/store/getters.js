23f3c09970e185a246860807a959a64c4fc4ccbc
liyuan <1481264084@qq.com>
2022-07-16 19:46:16 +0800

    完成了公司设置的基本功能

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   token: state => state.user.token, // 建立token的快捷访问
   name: state => state.user.userInfo.username, // 建立用户名的快捷访问
   userId: state => state.user.userInfo.userId, // 建立用户ID的快捷访问
-  staffPhoto: state => state.user.userInfo.staffPhoto // 建立头像的快捷访问
+  staffPhoto: state => state.user.userInfo.staffPhoto, // 建立头像的快捷访问
+  companyId: state => state.user.userInfo.companyId // 建立对于user模块的companyId的快捷访问
 }
 export default getters
