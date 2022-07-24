f24c8cdeded219679ed5d48817ec3ce97d3045b6
liyuan <1481264084@qq.com>
2022-07-24 13:01:24 +0800

    完成了主页模块的功能、动态主题的实现、多页签功能的实现

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,17 @@ import PageTools from './PageTools/index.vue'
 import UploadExcel from './UploadExcel/index.vue'
 import ImageUpload from './ImageUpload'
 import Print from 'vue-print-nb'
+import ScreenFull from './ScreenFull'
+import ThemePicker from './ThemePicker'
+import TagsView from './TagsView'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools) //  注册全局的通用栏组件对象
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload) // 注册导入上传组件
+    Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
+    Vue.component('ThemePicker', ThemePicker) // 注册颜色组件
+    Vue.component('TagsView', TagsView) // 注册多页签组件
     Vue.use(Print) // 注册打印插件
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -19,10 +18,7 @@ import * as filters from '@/filters' // 引入工具类
 import '@/icons' // icon
 import '@/permission' // permission control
 
-// set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 Object.keys(directives).forEach(key => {
   Vue.directive(key, directives[key]) // 注册自定义指令
